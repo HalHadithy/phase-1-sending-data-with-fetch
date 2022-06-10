1b13f563fac3942c77767a319dcfeaa3fe6d88c1
Haala <haalahadithy@gmail.com>
2022-06-10 13:45:17 -0400

    commit tree ce29479c905f3bde8cdddbb66a58f44aa4a4dad3

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Add your code here
+
+//Event Listner
+//document.querySelector('')
+
+
+//set a JavaScript arguement that will be used to change default's behavior 
+const formData ={
+    dogName: "Byron",
+    dogBreed: "Poodle",
+};
+
+const configObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept" : "application/json",
+    },
+    body: JSON.stringify({formData}),
+}
+
+//fetch to the dogs object from the local host 
+fetch('http://localhost:3000/dogs', configObject)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+    })
+    .catch(function(error){
+        alert("Something went wrong!");
+        console.log(error.message);
+    });
+
+/* alternative way of writing the .then:
+    .then (response => response.json() )
+    .then (object => console.log(object))
+*/
+
+const deleteObject = {
+    method: "DELETE",
+   
+}
+
+
+
+function submitData(){
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify({ 
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    }
+
+    fetch('http://localhost:3000/users', configObject)
+        .then (response => response.json())
+        .then (object => document.body.append(object.id))
+        .catch(function(error){
+            document.body.append("Unauthorized Access")
+        });
+
+}
